91ac5bff7ea14cc25e5cc0e42defab2ad036f635
CEzequielR <checheze@hotmail.com.ar>
2024-08-20 19:15:50 -0300

    Primera tarea, estructurar una página básica

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Página Web</title>
+</head>
+<body>
+    <header>
+        <h1>Programación IV</h1>
+        <div class="menu">
+        <a class="intro" href="#introduccion"> Introduccion</a>
+        <a class="conte" href="#contenido"> Contenido</a>
+        </div>
+    </header>
+    <div id="introduccion">
+        <h2>
+            Soy estudiante, me llamo Ezequiel Roldán y esta es una página de presentación para probar conocimientos básicos de HTML, CSS y php.
+        </h2>
+    </div>
+            <div id="contenido">
+                <h3>Tecnologías que uso en este ejemplo.</h3>
+                <div class="logos">
+            <img class="imagenes" src="Imagenes\css.png" alt="Logo Css">
+            <img class="imagenes" src="Imagenes\html.png" alt="Logo Html">
+            <img class="imagenes" src="Imagenes\php.png" alt="Logo php">
+                </div>
+            </div>
+</body>
+</html>
